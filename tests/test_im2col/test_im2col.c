16bd8f0eb651f825f0c25762796eabd33c2763fa
SixDouble0 <162843064+SixDouble0@users.noreply.github.com>
2026-04-04 02:51:39 +0200

    All tests passed

--- tests/test_im2col/test_im2col.c
+++ tests/test_im2col/test_im2col.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include "time.h"
+#include "esp_timer.h" // ESP32 high-resolution timer
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <unity.h>
 #include "../../src/operations/im2col.h"
 #include "../../src/operations/conv2d.h"
@@ -99,34 +101,42 @@ void benchmark_im2col_vs_conv2d(void) {
     // bigger input to showcase a difference in performance, e.g. 28x28 like MNIST
     // np. 28x28 like MNIST
     static int8_t input[784];
-    static int8_t weights[288]; // 32 filters 3x3x1
-    static int32_t output_naive[26*26*32];
+    static int8_t weights[144]; // 16 filters 3x3x1
+    static int32_t output_naive[26*26*16];
     static int8_t col[26*26*9];
-    static int32_t output_fast[26*26*32];
+    static int32_t output_fast[26*26*16];
 
-    clock_t start, end; 
+    int64_t start, end; 
 
     // Initialize input and weights with some values
     for (int i = 0; i < 784; i++) input[i] = (int8_t)(i % 128);
-    for (int i = 0; i < 288; i++) weights[i] = (int8_t)(i % 64);
+    for (int i = 0; i < 144; i++) weights[i] = (int8_t)(i % 64);
 
     // naive conv2d
-    start = clock();
-    for (int i = 0; i < 1000; i++)
-        conv2d(input, weights, output_naive, 28, 28, 1, 32, 3, 3, 1, 0);
-    end = clock();
-    printf("conv2d naive: %f ms\n", (double)(end-start)/CLOCKS_PER_SEC*1000/1000);
+    start = esp_timer_get_time();
+    for (int i = 0; i < 20; i++)
+        conv2d(input, weights, output_naive, 28, 28, 1, 16, 3, 3, 1, 0);
+    end = esp_timer_get_time();
+    
+    char msg1[64];
+    sprintf(msg1, "conv2d naive: %lld microseconds", (end-start) / 20);
+    TEST_MESSAGE(msg1);
 
     // im2col + gemm
-    start = clock();
-    for (int i = 0; i < 1000; i++) {
-    im2col(input, col, 28, 28, 1, 3, 3, 1, 0);
-    gemm_optimized(col, weights, output_fast, 26*26, 32, 9);
+    start = esp_timer_get_time();
+    for (int i = 0; i < 20; i++) {
+        im2col(input, col, 28, 28, 1, 3, 3, 1, 0);
+        gemm_optimized(col, weights, output_fast, 26*26, 16, 9);
     }
-    end = clock();
-    printf("im2col+gemm:  %f ms\n", (double)(end-start)/CLOCKS_PER_SEC*1000/1000);
+    end = esp_timer_get_time();
+
+    char msg2[64];
+    sprintf(msg2, "im2col+gemm: %lld microseconds", (end-start) / 20);
+    TEST_MESSAGE(msg2);
 }
 void app_main(void){
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
     UNITY_BEGIN();
     RUN_TEST(im2col_test);
     RUN_TEST(im2col_vs_conv2d_test);
